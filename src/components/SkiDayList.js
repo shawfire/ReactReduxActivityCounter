a56689f9d9da4402891c985f26207916cd462d22
John Shaw <shaw_fire@yahoo.com>
2017-03-13 10:18:50 +1100

    Custom prop type using a function.

--- src/components/SkiDayList.js
+++ src/components/SkiDayList.js
@@ -25,5 +25,17 @@ export const SkiDayList = ({days}) => (
 )
 
 SkiDayList.propTypes = {
-	days: PropTypes.array
+	days: function(props) {
+		if(!Array.isArray(props.days)) {
+			return new Error(
+				"SkiDayList should be an array"
+				)
+		} else if(!props.days.length) {
+			return new Error(
+				"SkiDayList must have at least one record"
+				)
+		} else {
+			return null
+		}
+	}
 }
