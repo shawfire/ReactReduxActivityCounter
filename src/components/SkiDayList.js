1e85e20906c5cf16839c8860599ab3b60b5f610f
John Shaw <shaw_fire@yahoo.com>
2017-03-13 10:17:16 +1100

    Add react prop types.

--- src/components/SkiDayList.js
+++ src/components/SkiDayList.js
@@ -2,6 +2,7 @@ import Terrain from 'react-icons/lib/md/terrain'
 import SnowFlake from 'react-icons/lib/ti/weather-snow'
 import Calendar from 'react-icons/lib/fa/calendar'
 import { SkiDayRow } from './SkiDayRow'
+import { PropTypes } from 'react'
 
 export const SkiDayList = ({days}) => (
 	<table>
@@ -16,16 +17,13 @@ export const SkiDayList = ({days}) => (
 		<tbody>
 			{days.map((day, i) =>
 				<SkiDayRow key={i}
-						   {...day}/>	
+						   {...day}/>
 				)}
 		</tbody>
 
 	</table>
 )
 
-
-
-
-
-
-
+SkiDayList.propTypes = {
+	days: PropTypes.array
+}
