2fba8d1b6b861f3380c6dc0724e07f6750b041e8
John Shaw <shaw_fire@yahoo.com>
2017-03-13 13:02:51 +1100

    Switch back to SkiDayCounterApp.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
-import routes from './routes'
+import './stylesheets/ui.scss'
+import { App } from './components/App'
+import { Whoops404 } from './components/Whoops404'
+import { Router, Route, hashHistory } from 'react-router'
 
 window.React = React
 
-render(routes, document.getElementById('react-container'))
+render(
+	<Router history={hashHistory}>
+		<Route path="/" component={App}/>
+		<Route path="list-days" component={App}>
+			<Route path=":filter" component={App} />
+		</Route>
+		<Route path="add-day" component={App} />
+		<Route path="*" component={Whoops404}/>
+	</Router>,
+	document.getElementById('react-container')
+)
+
